11981e2b87089690f96f6661f683b3c56e40504c
Cristian Cáceres <ccaceresm1@miumg.edu.gt>
2023-06-22 20:13:04 -0600

    se quita funcion home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,24 +9,8 @@ import * as moment from 'moment';
 })
 export class HomePage implements OnInit {
 
-  fecha: string;
 
-  entradas: Array<{
-    fecha: string,
-    fechaTexto: string,
-    texto: string
-  }>
-
-  entradaActual: {
-    fecha: string,
-    fechaTexto: string,
-    texto: string
-  };
-
-  constructor(public toastController: ToastController) {
-    moment.locale('es-mx');
-    this.fecha = moment().format();
-    this.cargarEntradas();
+  constructor() {
 
   }
 
@@ -35,69 +19,4 @@ export class HomePage implements OnInit {
 
 
 
-  cargarEntradas(){
-    var fecha = moment(this.fecha).format('DD-MM-YY');
-
-    this.entradas = JSON.parse(localStorage.getItem('entradas'));
-    if(this.entradas){
-      var entradaActual = this.entradas.find((elemento)=>{
-        return elemento.fecha == fecha;
-      });
-      if(entradaActual){
-        this.entradaActual = entradaActual;
-      }else{
-        this.inicializarNuevaEntrada();
-      }
-    }else{
-      this.inicializarNuevaEntrada();
-    }
-  }
-
-  inicializarNuevaEntrada(){
-    var fecha = moment(this.fecha).format('DD-MM-YY');
-    var dia = moment(this.fecha).format('DD');
-    var mes = moment(this.fecha).format('MMMM');
-    var year = moment(this.fecha).format('YYYY');
-
-    this.entradaActual = {
-      fechaTexto: dia + ' de ' + mes + ' del ' +  year,
-      fecha: fecha,
-      texto: ''
-    }
-  }
-
-  async guardar(entradaActual: {
-    fecha: string,
-    fechaTexto: string,
-    texto: string
-  }){
-
-    var fecha = moment(this.fecha).format('DD-MM-YY');
-
-    if(this.entradas){
-      var item = this.entradas.find((elemento)=>{
-        return elemento.fecha == fecha;
-      });
-      if(item){
-        localStorage.setItem('entradas',JSON.stringify(this.entradas));
-      }else{
-        this.guardarItem(entradaActual);
-      }
-
-    }else{
-      this.entradas = [];
-      this.guardarItem(entradaActual);
-    }
-
-    const toast = await this.toastController.create({
-      message: 'Datos guardados',
-      duration: 2000
-    });
-    toast.present();
-  }
-
-  guardarItem(entrada:{fecha: string,fechaTexto: string,texto: string }){
-    this.entradas.push(entrada);
-    localStorage.setItem('entradas',JSON.stringify(this.entradas));
-  }
 }
